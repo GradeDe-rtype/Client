ba0eea936ddbdf84e057b9538d054e67b7db9abc
dragusheen <nathan.tirolf@epitech.eu>
2024-12-09 12:12:53 +0100

    [+] Add font

---
+++ lib/gradeDe/src/Font.cpp
@@ -0,0 +1,19 @@
+/*
+    Authors:
+    >> Nathan TIROLF - { nathan.tirolf@epitech.eu }
+
+    („• ֊ •„)❤  <  Have a good day !
+    --U-----U------------------------
+*/
+
+/*  ---- INCLUDES ---- */
+#include "GradeDe/Font.hpp"
+
+namespace gd
+{
+    void Font::load(const std::string &path)
+    {
+        if (!_font.loadFromFile(path))
+            throw Error("Failed to load font", path);
+    }
+} // namespace gd
