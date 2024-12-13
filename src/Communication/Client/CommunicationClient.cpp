680c75e5294a4c1c3fd9a6602aab4e8d19385562
Dan13615 <daniil.stepanov@epitech.eu>
2024-12-13 19:52:06 +0100

    [~] working on async I/O

--- src/Communication/Client/CommunicationClient.cpp
+++ src/Communication/Client/CommunicationClient.cpp
@@ -15,6 +15,7 @@ namespace RType
         Client::Client(std::string ip, int port, std::shared_ptr<RType::Communication::SendList> sendList)
             : _ip(ip), _port(port), _sendList(sendList), _state(TRY_CONNECT), _socket(_io_context)
         {
+            _connect();
         }
 
         void Client::run()
@@ -82,30 +83,48 @@ namespace RType
         {
             try {
                 if (_socket.available() == 0) return;
-                boost::asio::streambuf streambuf;
-                boost::asio::read_until(_socket, streambuf, "\n");
-                std::istream is(&streambuf);
-                std::string line;
-                while (std::getline(is, line)) {
-                    std::string trimmed = RType::Helpers::Utils::trim(line);
-                    if (!trimmed.empty()) _buffer.push_back(trimmed);
-                }
-            } catch (const std::exception &e) {
+                std::array<char, sizeof(rfcArgParser::DataPacket)> buffer;
+                std::cout << "Reading..." << std::endl;
+                boost::asio::read(_socket, boost::asio::buffer(buffer, 16));
+                std::cout << "Received:" << std::endl;
+                std::string data(buffer.begin(), buffer.end());
+                rfcArgParser::DataPacket packet = rfcArgParser::DeserializePacket(data, sizeof(data));
+                std::cout << "Command: " << packet.command << "Args :" << packet.args << std::endl;
+                _buffer.push_back(rfcArgParser::DeserializePacket(packet));
+                // _read();
+            } catch (const boost::system::system_error &e) {
                 std::cerr << "Read error: " << e.what() << std::endl;
+                _state = DISCONNECT;
+            } catch (const std::exception &e) {
+                std::cerr << "Unexpected error: " << e.what() << std::endl;
                 _state = DISCONNECT;
             }
         }
 
+
         void Client::_write()
         {
             if (_sendList->size() > 0) {
+                int i = 0;
+                std::string args;
                 std::vector<std::string> messages = _sendList->pop();
+                std::string command;
+                rfcArgParser::DataPacket packet{};
                 for (std::string message : messages) {
-                    message += "\r\n";
-                    boost::asio::write(_socket, boost::asio::buffer(message));
+                    command = message.substr(0, message.find(" "));
+                    args = message.substr(message.find(" ") + 1);
+                    packet = rfcArgParser::SerializePacket(command, args);
+                    try {
+                        boost::asio::write(
+                            _socket, boost::asio::buffer(&packet, sizeof(packet)));
+                    } catch (const boost::system::system_error &e) {
+                        std::cerr << "Write error: " << e.what() << std::endl;
+                        _state = DISCONNECT;
+                    }
                 }
             }
         }
+
 
         void Client::_checkOpen()
         {
